15a57fd6751f81c377fa031240555b0a545921f2
Chase Manning <chase@manning.dev>
2024-06-30 22:00:36 +0100

    :sparkles: add time conversion functions

--- src/app/addMinutes.ts
+++ src/app/addMinutes.ts
@@ -1,11 +1,22 @@
+export const timeToMinutes = (time: number): number => {
+  const hours = Math.floor(time / 100);
+  const minutes = time % 100;
+  return hours * 60 + minutes;
+};
+
+export const minutesToTime = (minutes: number): number => {
+  const hours = Math.floor(minutes / 60);
+  const newMinutes = minutes % 60;
+  return hours * 100 + newMinutes;
+};
+
 const addMinutes = (time: number, minutes: number): number => {
-  const timeHours = Math.floor(time / 100);
-  const timeMinutes = time % 100;
-  const totalTime = timeHours * 60 + timeMinutes;
-  const newTime = totalTime + minutes;
-  const newTimeHours = Math.floor(newTime / 60);
-  const newTimeMinutes = newTime % 60;
-  return newTimeHours * 100 + newTimeMinutes;
+  const totalTime = timeToMinutes(time);
+  let newTime = totalTime + minutes;
+  if (newTime >= 1440) {
+    newTime -= 1440;
+  }
+  return minutesToTime(newTime);
 };
 
 export default addMinutes;
