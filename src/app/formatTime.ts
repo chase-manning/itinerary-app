3538d68cd3f8bb2a59bed4152521d2de6084cec2
Chase Manning <chase@manning.dev>
2024-06-30 22:00:47 +0100

    :lipstick: round times to 5 minutes

--- src/app/formatTime.ts
+++ src/app/formatTime.ts
@@ -1,6 +1,8 @@
 const formatTime = (time: number): string => {
   let hours = Math.floor(time / 100);
-  const minutes = time % 100;
+  let minutes = time % 100;
+  // Round minutes to the nearest 5
+  minutes = Math.round(minutes / 5) * 5;
   const ampm = hours >= 12 ? "pm" : "am";
   hours = hours % 12;
   return `${hours}:${minutes.toString().padStart(2, "0")} ${ampm}`;
