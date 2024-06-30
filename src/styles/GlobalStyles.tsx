1f615e72ba7e86ec21a5c86aceedfa12e19420b7
Chase Manning <chase@manning.dev>
2024-06-30 17:07:04 +0100

    :lipstick: update main color

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
     :root {
         --bg: white;
-        --main: black;
+        --main: rgba(0,0,0,0.7);
         --sub: grey;
         --primary: blue;
 
