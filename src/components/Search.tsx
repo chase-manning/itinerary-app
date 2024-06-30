9f3dda95ae31164219d715f26ab751e3d025fd50
Chase Manning <chase@manning.dev>
2024-06-30 16:34:32 +0100

    :sparkles: create search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const StyledSearch = styled.div`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  padding: 0 1rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: solid 1px var(--sub);
+  height: 2rem;
+  border-radius: 1rem;
+  color: var(--main);
+
+  ::placeholder {
+    color: var(--sub);
+  }
+`;
+
+interface Props {
+  value: string;
+  setValue: (value: string) => void;
+}
+
+const Search = ({ value, setValue }: Props) => {
+  return (
+    <StyledSearch>
+      <Input
+        type="text"
+        placeholder="Search..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </StyledSearch>
+  );
+};
+
+export default Search;
