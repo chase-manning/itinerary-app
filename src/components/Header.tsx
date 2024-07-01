6a8d1a46388c3fe20b898651f0e501166777093e
Chase Manning <chase@manning.dev>
2024-07-01 21:02:37 +0100

    :sparkles: create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import getPersonPfp from "../app/getPersonPfp";
+import { PersonType } from "../app/itinerary";
+
+const StyledHeader = styled.div`
+  width: 100%;
+  height: 8rem;
+  display: flex;
+  padding: 0 2rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Hello = styled.div`
+  font-size: 2.7rem;
+  font-weight: 600;
+`;
+
+const ProfilePicButton = styled.button`
+  cursor: pointer;
+  border-radius: 50%;
+  overflow: hidden;
+`;
+
+const ProfilePic = styled.img`
+  height: 4rem;
+`;
+
+interface Props {
+  person: PersonType;
+}
+
+const Header = ({ person }: Props) => {
+  return (
+    <StyledHeader>
+      <Hello>{`Hello, ${person}!`}</Hello>
+      <ProfilePicButton>
+        <ProfilePic src={getPersonPfp(person)} />
+      </ProfilePicButton>
+    </StyledHeader>
+  );
+};
+
+export default Header;
